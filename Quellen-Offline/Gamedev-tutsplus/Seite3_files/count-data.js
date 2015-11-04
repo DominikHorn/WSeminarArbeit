19c68743376c44f5a4063949c39459b38dae0aee
Dominik Horn <horn.dominik101@yahoo.de>
2015-11-04 08:49:42 +0100

    2.3.2 WIP
    * Adds new source
    * Moves Abb to separate folder
    * Adds all sources in offline format

---
+++ Quellen-Offline/Gamedev-tutsplus/Seite3_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"7756 http:\/\/gamedev.tutsplus.com\/?p=7756","comments":19}]});
+}
